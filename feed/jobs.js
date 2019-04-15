e7ca480aa16ed89ba2949836f2fa46ef0d3a80c3
nina hedman <ni.hedman@gmail.com>
2019-04-15 16:14:16 +0200

    Added gitignore for Node modules. Removed seed folder and ads_seeds. Jobs.js: Data that is retrieved from feed is saved to db.

--- feed/jobs.js
+++ feed/jobs.js
@@ -1,27 +1,40 @@
 const request = require("superagent");
-// log all events to the terminal
+const knex = require("../knex/knex");
 
-// GET JOBS FROM FEED
-module.exports = ({ jobsRouter }) => {
-  jobsRouter.get("/", async (ctx, next) => {
+// GET JOBS FROM FEED AND SAVE TO DB
+module.exports = ({ router }) => {
+  router.get("/", async (ctx, next) => {
     await request
       .get("https://api.workbuster.com/jobs/feed/kyhdemo?format=json")
       .then(res => {
+        const jobs = res.body.jobs;
+
+        // GET JOB DATA FROM FEED
         const jobObject = {
-          id: res.body.jobs[0].id,
-          title: res.body.jobs[0].title,
-          description_short: res.body.jobs[0].description_short,
-          last_application_timestamp:
-            res.body.jobs[0].last_application_timestamp,
-          published_first_date: res.body.jobs[0].published_first_date,
-          updated_timestamp: res.body.jobs[0].updated_timestamp,
-          apply_url: res.body.jobs[0].apply_url,
-          image: res.body.jobs[0].image,
-          company: res.body.jobs[0].company.name,
-          city: res.body.jobs[0].location.city
+          id: jobs[0].id,
+          title: jobs[0].title,
+          description_short: jobs[0].description_short,
+          last_application_timestamp: jobs[0].last_application_timestamp,
+          published_first_date: jobs[0].published_first_date,
+          updated_timestamp: jobs[0].updated_timestamp,
+          apply_url: jobs[0].apply_url,
+          image: jobs[0].image,
+          company: jobs[0].company.name,
+          city: jobs[0].company.city,
+          views: 0,
+          clicks: 0,
+          applies: 0
         };
-        console.log("object", jobObject);
-        ctx.body = [jobObject];
+        console.log("object", jobObject); // REMOVE LATER ON
+
+        // ADD JOB DATA TO DATABASE
+        knex("ads")
+          .insert(jobObject)
+          .then(function(result) {
+            // .then required so that promise is executed
+            res.json({ success: true, message: "ok" }); // respond back to request
+          });
+        ctx.body = [jobObject]; // REMOVE LATER ON
       })
 
       .catch(err => {
