a551011118e8015f3fa0130eccc222620f6c6a22
nina hedman <ni.hedman@gmail.com>
2019-04-23 17:03:01 +0200

    Add polling (interval) to check for new jobs in feed and save to db

--- feed/jobs.js
+++ feed/jobs.js
@@ -1,10 +1,14 @@
 const request = require("superagent");
 const knex = require("../knex/knex");
 
+const AsyncPolling = require("async-polling");
+const checkNewJobsInterval = 3000; //3000 = 3s
 const feed = "https://api.workbuster.com/jobs/feed/kyhdemo?format=json";
 
-// JOBS = ALL OBJECTS IN FEED
-// ADS = ALL OBJECTS IN DATABASE = DATA OF BOTH JOB OBJECT AND AD INFO
+// ===========================================================================//
+//    JOBS = ALL OBJECTS IN FEED                                              //
+//    ADS = ALL OBJECTS IN DATABASE = DATA OF BOTH JOB OBJECT AND AD INFO     //
+// ===========================================================================//
 
 let allJobsID = []; // ID's of all jobs in feed
 let allNewJobsIndex = []; // All jobs that are in feed but not in database
@@ -12,123 +16,137 @@ let allAds = []; // Object of each ad in database
 let allAdsID = []; // ID's of all ads in database
 
 let allResults = []; // Array with allJobsID, allAds, AllAdsID, allNewJobsIndex // REMOVE LATER ON
+let pollingRuns = 0; // REMOVE LATER ON
 
-module.exports = ({ router }) => {
-  router.get("/", async (ctx, next) => {
-    // ======================= //
-    //     GET ALL JOBS ID     // GET ID OF EACH JOB IN FEED, SAVE TO ARRAY allJobsID
-    // ======================= //
+// ========================================================= //
+//               ON GIVEN INTERVAL                           //
+//       CHECK FOR NEW JOBS IN FEED AND SAVE TO DATABASE     //
+// ========================================================= //
 
-    await request
-      .get(feed)
-      .then(res => {
-        const jobs = res.body.jobs;
-
-        var i;
-        for (i = 0; i < jobs.length; i++) {
-          let id = jobs[i].id;
-          let job = id;
-          allJobsID.push(job);
-        }
-        allResults.push(allJobsID); // REMOVE LATER ON
-        ctx.body = allJobsID; // REMOVE LATER ON
-      })
-      .catch(err => {
-        return ctx.throw(400, "no data to get the api,");
-      });
-
-    // ======================= //
-    //     GET ALL ADS         // GET OBJECT OF EACH AD IN DATABASE, SAVE TO ARRAY allAds
-    // ======================= //
-
-    allAds = await knex("ads");
-    ctx.body = allAds; // REMOVE LATER ON
-    allResults.push(allAds); // REMOVE LATER ON
-
-    // ======================= //
-    //     GET ALL ADS ID      // GET ID OF EACH AD IN DATABASE, SAVE TO ARRAY allAdsID
-    // ======================= //
-
-    allAds = await knex.select("id").from("ads");
-    allAdsID = []; // Epmty array to get a fresh one
-    for (i = 0; i < allAds.length; i++) {
-      let adID = allAds[i].id;
-
-      allAdsID.push(adID);
-    }
-    allResults.push(allAdsID); // REMOVE LATER ON
-    ctx.body = allAdsID; // REMOVE LATER ON
-
-    // =========================== //
-    //    GET ALL NEW JOBS INDEX   // GET INDEX OF UNSAVED JOBS (exists in feed but not in database), SAVE TO ARRAY allNewJobsIndex
-    // =========================== //
-
-    allNewJobsIndex = []; // Epmty array to get a fresh one
-    for (i = 0; i < allJobsID.length; i++) {
-      let id = allJobsID[i];
-
-      if (!allAdsID.includes(id)) {
-        allNewJobsIndex.push(i);
-      }
-    }
-    allResults.push(allNewJobsIndex); // REMOVE LATER ON
-
-    ctx.body = allNewJobsIndex; // REMOVE LATER ON
-    ctx.body = allResults; // REMOVE LATER ON
-
-    // =========================== //
-    //    IF THERE ARE NEW JOBS    //
-    // =========================== //
-
-    if (!allNewJobsIndex) {
-      // =========================== //
-      //    GET EACH NEW JOBS DATA   // GET DATA FROM FEED FOR EACH JOB IN FEED THAT IS NOT SAVED IN DATABASE (based in allNewJobsIndex)
-      // =========================== //
+AsyncPolling(function(end) {
+  pollingRuns++; // REMOVE LATER ON
+  // console.log( pollingRuns ); // REMOVE LATER ON
+  module.exports = ({ router }) => {
+    router.get("/", async (ctx, next) => {
+      // ======================= //
+      //     GET ALL JOBS ID     // GET ID OF EACH JOB IN FEED, SAVE TO ARRAY allJobsID
+      // ======================= //
 
       await request
         .get(feed)
         .then(res => {
           const jobs = res.body.jobs;
-          let jobObject;
 
-          allNewJobsIndex.forEach(function(index) {
-            jobObject = {
-              id: jobs[index].id,
-              title: jobs[index].title,
-              description_short: jobs[index].description_short,
-              last_application_timestamp:
-                jobs[index].last_application_timestamp,
-              published_first_date: jobs[index].published_first_date,
-              updated_timestamp: jobs[index].updated_timestamp,
-              apply_url: jobs[index].apply_url,
-              image: jobs[index].image,
-              company: jobs[index].company.name,
-              city: jobs[index].company.city || "Ospecificerad stad",
-              views: 0,
-              clicks: 0,
-              applies: 0
-            };
-
-            // ================================== //
-            //    ADD ALL NEW JOBS TO DATABASE    // SAVE EACH JOB OBJECT TO DATABASE
-            // ================================== //
-
-            knex("ads")
-              .insert(jobObject)
-              .then(function(result) {
-                // .then required so that promise is executed
-                res.json({ success: true, message: "ok" }); // respond back to request
-              });
-
-            ctx.body = [jobObject]; // REMOVE LATER ON
-          });
+          var i;
+          for (i = 0; i < jobs.length; i++) {
+            let id = jobs[i].id;
+            let job = id;
+            allJobsID.push(job);
+          }
+          allResults.push(allJobsID); // REMOVE LATER ON
+          ctx.body = allJobsID; // REMOVE LATER ON
         })
-
         .catch(err => {
           return ctx.throw(400, "no data to get the api,");
         });
-    } else {
-      ctx.body = "There are no new jobs in the feed"; // REMOVE LATER ON
-    }
-  });
-};
+
+      // ======================= //
+      //     GET ALL ADS         // GET OBJECT OF EACH AD IN DATABASE, SAVE TO ARRAY allAds
+      // ======================= //
+
+      allAds = await knex("ads");
+      ctx.body = allAds; // REMOVE LATER ON
+      allResults.push(allAds); // REMOVE LATER ON
+
+      // ======================= //
+      //     GET ALL ADS ID      // GET ID OF EACH AD IN DATABASE, SAVE TO ARRAY allAdsID
+      // ======================= //
+
+      allAds = await knex.select("id").from("ads");
+      allAdsID = []; // Epmty array to get a fresh one
+      for (i = 0; i < allAds.length; i++) {
+        let adID = allAds[i].id;
+
+        allAdsID.push(adID);
+      }
+      allResults.push(allAdsID); // REMOVE LATER ON
+      ctx.body = allAdsID; // REMOVE LATER ON
+
+      // =========================== //
+      //    GET ALL NEW JOBS INDEX   // GET INDEX OF UNSAVED JOBS (exists in feed but not in database), SAVE TO ARRAY allNewJobsIndex
+      // =========================== //
+
+      allNewJobsIndex = []; // Epmty array to get a fresh one
+      for (i = 0; i < allJobsID.length; i++) {
+        let id = allJobsID[i];
+
+        if (!allAdsID.includes(id)) {
+          allNewJobsIndex.push(i);
+        }
+      }
+      allResults.push(allNewJobsIndex); // REMOVE LATER ON
+
+      ctx.body = allNewJobsIndex; // REMOVE LATER ON
+      ctx.body = allResults; // REMOVE LATER ON
+
+      // =========================== //
+      //    IF THERE ARE NEW JOBS    //
+      // =========================== //
+
+      if (!allNewJobsIndex) {
+        // =========================== //
+        //    GET EACH NEW JOBS DATA   // GET DATA FROM FEED FOR EACH JOB IN FEED THAT IS NOT SAVED IN DATABASE (based in allNewJobsIndex)
+        // =========================== //
+
+        await request
+          .get(feed)
+          .then(res => {
+            const jobs = res.body.jobs;
+            let jobObject;
+
+            allNewJobsIndex.forEach(function(index) {
+              jobObject = {
+                id: jobs[index].id,
+                title: jobs[index].title,
+                description_short: jobs[index].description_short,
+                last_application_timestamp:
+                  jobs[index].last_application_timestamp,
+                published_first_date: jobs[index].published_first_date,
+                updated_timestamp: jobs[index].updated_timestamp,
+                apply_url: jobs[index].apply_url,
+                image: jobs[index].image,
+                company: jobs[index].company.name,
+                city: jobs[index].company.city || "Ospecificerad stad",
+                views: 0,
+                clicks: 0,
+                applies: 0
+              };
+
+              // ================================== //
+              //    ADD ALL NEW JOBS TO DATABASE    // SAVE EACH JOB OBJECT TO DATABASE
+              // ================================== //
+
+              knex("ads")
+                .insert(jobObject)
+                .then(function(result) {
+                  // .then required so that promise is executed
+                  res.json({ success: true, message: "ok" }); // respond back to request
+                });
+
+              ctx.body = [jobObject]; // REMOVE LATER ON
+            });
+          })
+
+          .catch(err => {
+            return ctx.throw(400, "no data to get the api,");
+          });
+      } else {
+        ctx.body = "There are no new jobs in the feed"; // REMOVE LATER ON
+      }
+
+      ctx.body += "         polling runs: " + pollingRuns; // REMOVE LATER ON
+    });
+  };
+
+  end();
+}, checkNewJobsInterval).run();
