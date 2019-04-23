d1eede231dbd40f019dda464e728ededc5f0cc96
nina hedman <ni.hedman@gmail.com>
2019-04-23 14:01:31 +0200

    Add function to get index of all new jobs in feed (that is not yet in database)

--- feed/jobs.js
+++ feed/jobs.js
@@ -7,23 +7,22 @@ const feed = "https://api.workbuster.com/jobs/feed/kyhdemo?format=json";
 // JOBS = ALL OBJECTS IN FEED
 // ADS = ALL OBJECTS IN DATABASE = DATA OF BOTH JOB OBJECT AND AD INFO
 
-let allJobsID = []; // ID's and index of all jobs in feed
+let allJobsID = []; // ID's of all jobs in feed
 let allNewJobsIndex = []; // All jobs that are in feed but not in database
 let allAds = []; // Object of each ad in database
 let allAdsID = []; // ID's of all ads in database
 
 module.exports = ({ router }) => {
   // --- GET ALL JOBS ID --- //
-  // GET INDEX AND ID OF EACH JOB IN FEED, SAVE TO ARRAY allJobsID
+  // GETID OF EACH JOB IN FEED, SAVE TO ARRAY allJobsID
   router.get("/jobslength", async (ctx, next) => {
     await request.get(feed).then(res => {
       const jobs = res.body.jobs;
 
       var i;
       for (i = 0; i < jobs.length; i++) {
-        let index = i;
         let id = jobs[i].id;
-        let job = { index, id };
+        let job = id;
         allJobsID.push(job);
 
         ctx.body = allJobsID; // REMOVE LATER ON
@@ -41,47 +40,30 @@ module.exports = ({ router }) => {
   // --- GET ALL ADS ID --- //
   // GET ID OF EACH AD IN DATABASE, SAVE TO ARRAY allAdsID
   router.get("/ID", async (ctx, next) => {
-    allAdsID = await knex.select("id").from("ads");
-    ctx.body = allAdsID; // REMOVE LATER ON
+    allAds = await knex.select("id").from("ads");
+    allAdsID = []; // Epmty array to get a fresh one
+    for (i = 0; i < allAds.length; i++) {
+      let adID = allAds[i].id;
+
+      allAdsID.push(adID);
+      ctx.body = allAdsID; // REMOVE LATER ON
+    }
   });
 
   // --- GET ALL NEW JOBS INDEX--- //
   // GET INDEX OF UNSAVED JOBS (exists in feed but not in database), SAVE TO ARRAY allNewJobsIndex
+  router.get("/newJobs", async (ctx, next) => {
+    allNewJobsIndex = []; // Epmty array to get a fresh one
+    let i;
+    for (i = 0; i < allJobsID.length; i++) {
+      let id = allJobsID[i];
 
-  // compare allAdsID and allJobsID
-  // remove all dublicates
-  // save index of each remaining ID
-
-  //
-  //
-  //
-  //
-  //
-  //
-
-  //
-  //
-  //
-  //
-  //
-
-  //
-  //
-  //
-  //
-  //
-
-  //
-  //
-  //
-  //
-  //
-
-  //
-  //
-  //
-  //
-  //
+      if (!allAdsID.includes(id)) {
+        allNewJobsIndex.push(i);
+      }
+    }
+    ctx.body = allNewJobsIndex; // REMOVE LATER ON
+  });
 
   // GET JOBS FROM FEED AND SAVE TO DB
   router.get("/", async (ctx, next) => {
