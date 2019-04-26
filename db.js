e1808daca4f1e048e5c970761f814adcb1282c65
nina hedman <ni.hedman@gmail.com>
2019-04-26 11:08:00 +0200

    Removes db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-// var config      = require('./knexfile.js');
-// var env         = 'development';
-// var knex        = require('knex')(config[env]);
-
-// module.exports = knex;
-
-// knex.migrate.latest([config]);
