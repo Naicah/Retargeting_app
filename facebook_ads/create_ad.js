76554d11c0a3080ca964ba62295f1cac8e87787e
Oliver Brian Barra Vasquez <oliver.vasquez@student.kyh.se>
2019-04-26 11:02:30 +0200

    changes in create and facebook

--- facebook_ads/create_ad.js
+++ facebook_ads/create_ad.js
@@ -1,207 +1,101 @@
-"use strict";
+// "use strict";
 
-const Koa = require("koa");
-const app = new Koa();
+// const Koa = require("koa");
+// const app = new Koa();
 
-const bizSdk = require("facebook-nodejs-business-sdk");
-const AdAccount = bizSdk.AdAccount;
-const Campaign = bizSdk.Campaign;
-const AdSet = bizSdk.AdSet;
+// const bizSdk = require("facebook-nodejs-business-sdk");
+// const AdAccount = bizSdk.AdAccount;
+// const Campaign = bizSdk.Campaign;
+// const AdSet = bizSdk.AdSet;
 
-<<<<<<< HEAD
+// const access_token =
+//   "EAAgSjZC0XzacBAChsNn9QFmCta44IgUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4KlavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD";
+// const app_secret = "2696e47e54e4f6f25248900e99f12fd3";
+// const app_id = "2272209449504167";
+// const id = "act_2268066353434439";
+// const api = bizSdk.FacebookAdsApi.init(access_token);
+// const showDebugingInfo = true; // Setting this to true shows more debugging info.
+// if (showDebugingInfo) {
+//   api.setDebug(true);
+// }
 
-const access_token = 'EAAgSjZC0XzacBAChsNn9QFmCta44IgUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4KlavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD';
-const app_secret = '2696e47e54e4f6f25248900e99f12fd3';
-const app_id = '2272209449504167';
-const id = 'act_2268066353434439';
-=======
-const access_token =
-  "EAAgSjZC0XzacBAChsNn9QFmCta44IgUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4KlavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD";
-const app_secret = "2696e47e54e4f6f25248900e99f12fd3";
-const app_id = "2272209449504167";
-const id = "act_2268066353434439";
->>>>>>> facebook
-const api = bizSdk.FacebookAdsApi.init(access_token);
-const showDebugingInfo = true; // Setting this to true shows more debugging info.
-if (showDebugingInfo) {
-  api.setDebug(true);
-}
+// // ================================== //
+// //         CREATE CAMPAIGN            //
+// // ================================== //
 
-<<<<<<< HEAD
-// const account = new AdAccount({'id': 'act_2268066353434439'})
-
-
-// todo: document everything
-=======
-// ================================== //
-//         CREATE CAMPAIGN            //
-// ================================== //
->>>>>>> facebook
-
-const logApiCallResult = (apiCallName, data) => {
-  console.log(apiCallName);
-  if (showDebugingInfo) {
-    // console.log('Data:' + JSON.stringify(data));
-  }
-};
-
-<<<<<<< HEAD
-
-// const adsetLogApiCallResult = (apiCallName, data) => {
+// const logApiCallResult = (apiCallName, data) => {
 //   console.log(apiCallName);
 //   if (showDebugingInfo) {
 //     // console.log('Data:' + JSON.stringify(data));
 //   }
 // };
 
-let campaignFields, campaignParams;
-campaignFields = [
-];
-let now = new Date();
+// let campaignFields, campaignParams;
+// campaignFields = [];
 
-campaignParams = {
-  
-  'name' : 'WORKBUSTER ' + now,
-  'objective' : 'LINK_CLICKS',
-  'status' : 'PAUSED',
-};
-
-// let adsetFields, adsetParams;
-// adsetFields = [
-// ];
-// adsetParams = {
-//   'name' : 'My AdSet',
-//   'optimization_goal' : 'REACH',
-//   'billing_event' : 'IMPRESSIONS',
-//   'bid_amount' : '2',
-//   'daily_budget' : '1000',
-//   'campaign_id' : '<adCampaignConversionsID>',
-//   'targeting' : {'geo_locations':{'countries':['US']},'behaviors':[{'id':6007101597783,'name':'Business Travelers'},{'id':6004386044572,'name':'Android Owners (All)'}]},
+// campaignParams = {
+//   name: new Date(),
+//   objective: "LINK_CLICKS",
+//   status: "PAUSED"
 // };
 
-// Custom audiences
+// // https://kyhdemo.workbuster.com/jobs/62892-frontendutvecklare-till-workbuster/thanks DENNA LÄNK AnVÄNDS SOM EXKLUDERINGS-PARAMETER"
+// let campaignID;
 
-// let customAudiencesFields, customAudiencesParams;
-// customAudiencesFields = [
-// ];
-// customAudiencesParams = {
-//  'name' : 'My Test Website Custom Audience',
-//  'rule' : {'inclusions':{'operator':'or','rules':[{'event_sources':[{'id':'<pixelID>','type':'pixel'}],'retention_seconds':8400,'filter':{'operator':'and','filters':[{'field':'url','operator':'i_contains','value':'shoes'}]}}]}},
-//  'prefill' : '1',
-// };
-// const customAudiences = (new AdAccount(id)).createCustomAudience(
-//  fields,
-//  params
-// );
-// logApiCallResult('customaudiences api call complete.', customAudiences);
+// const campaigns = new AdAccount(id)
+//   .createCampaign(campaignFields, campaignParams)
+//   .then(response => {
+//     campaignID = response.id;
+//     console.log("Här är campaignID't: " + campaignID);
+//   })
+//   .catch(err => {
+//     return response.throw(400, "no data to get the api,");
+//   });
 
-// https://kyhdemo.workbuster.com/jobs/62892-frontendutvecklare-till-workbuster/thanks DENNA LÄNK AnVÄNDS SOM EXKLUDERINGS-PARAMETER"
-let campaignID;
-const campaigns = (new AdAccount(id)).createCampaign(
-  campaignFields,
-  campaignParams
-).then(response => {
-   campaignID = response.id;
-   console.log("Här är ID't!" + campaignID)
-});
-
-
-// request("https://graph.facebook.com/v3.2/act_2268066353434439/campaigns?access_token=EAAgSjZC0XzacBAChsNn9QFmCta44IgUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4KlavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD", function(error, response, data) {
-//     body = data;
-// });
-
-// const adsets = (new AdAccount(id)).createAdSet(
-//   adsetFields,
-//   adsetParams
-// );
-
-// account.getCampaigns([campaigns.campaignFields], { limit: 10 }) // fields array and params
-// .then((campaigns) => {
-//   console.log(campaigns.length) // 10
-//   console.log(campaigns.hasNext()) // true
-//   return campaigns.next()
-// });
-
-
-logApiCallResult('campaigns api call complete.', campaigns);
-
-  // console.log(body);
-
-
-
-// adsetLogApiCallResult('adsets api call complete.', adsets);
-
-// https://graph.facebook.com/v3.2/act_2268066353434439/campaigns?access_token=EAAgSjZC0XzacBAChsNn9QFmCta44I
-// gUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4K
-// lavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD
-=======
-let campaignFields, campaignParams;
-campaignFields = [];
-
-campaignParams = {
-  name: new Date(),
-  objective: "LINK_CLICKS",
-  status: "PAUSED"
-};
-
-// https://kyhdemo.workbuster.com/jobs/62892-frontendutvecklare-till-workbuster/thanks DENNA LÄNK AnVÄNDS SOM EXKLUDERINGS-PARAMETER"
-let campaignID;
-
-const campaigns = new AdAccount(id)
-  .createCampaign(campaignFields, campaignParams)
-  .then(response => {
-    campaignID = response.id;
-    console.log("Här är campaignID't: " + campaignID);
-  })
-  .catch(err => {
-    return response.throw(400, "no data to get the api,");
-  });
-
-logApiCallResult("campaigns api call complete.", campaigns);
-
-// ================================== //
-//           CREATE ADSET             //
-// ================================== //
-
-const adsetLogApiCallResult = (apiCallName, data) => {
-  console.log(apiCallName);
-  if (showDebugingInfo) {
-    console.log("Data:" + JSON.stringify(data));
-  }
-};
-
-let adsetFields, adsetParams;
-adsetFields = [];
-adsetParams = {
-  name: "My AdSet",
-  campaign_id: campaignID,
-  optimization_goal: "REACH",
-  billing_event: "IMPRESSIONS",
-  bid_amount: "2",
-  daily_budget: "1000",
-
-  start_time: "2019-03-11T04:33:43-0700",
-  end_time: "2019-03-18T04:33:43-0700",
-
-  bid_amount: "1000",
-  promoted_object: { page_id: "<pageID>" },
-  targeting: { geo_locations: { countries: ["US"] } },
-  user_os: "iOS",
-  publisher_platforms: "facebook",
-  device_platforms: "mobile"
-};
-
-const adSets = new AdAccount(id)
-  .createAdSet(adsetFields, adsetParams)
-  .then(response => {
-    console.log("adset created");
-  })
-  .catch(err => {
-    return response.throw(400, "no data to get the api,");
-  });
-
-// logApiCallResult("campaigns api call complete.", camAdSet);
 // logApiCallResult("campaigns api call complete.", campaigns);
 
-adsetLogApiCallResult("adsets api call complete.", adSets);
->>>>>>> facebook
+// // ================================== //
+// //           CREATE ADSET             //
+// // ================================== //
+
+// const adsetLogApiCallResult = (apiCallName, data) => {
+//   console.log(apiCallName);
+//   if (showDebugingInfo) {
+//     console.log("Data:" + JSON.stringify(data));
+//   }
+// };
+
+// let adsetFields, adsetParams;
+// adsetFields = [];
+// adsetParams = {
+//   name: "My AdSet",
+//   campaign_id: campaignID,
+//   optimization_goal: "REACH",
+//   billing_event: "IMPRESSIONS",
+//   bid_amount: "2",
+//   daily_budget: "1000",
+
+//   start_time: "2019-03-11T04:33:43-0700",
+//   end_time: "2019-03-18T04:33:43-0700",
+
+//   bid_amount: "1000",
+//   promoted_object: { page_id: "<pageID>" },
+//   targeting: { geo_locations: { countries: ["US"] } },
+//   user_os: "iOS",
+//   publisher_platforms: "facebook",
+//   device_platforms: "mobile"
+// };
+
+// const adSets = new AdAccount(id)
+//   .createAdSet(adsetFields, adsetParams)
+//   .then(response => {
+//     console.log("adset created");
+//   })
+//   .catch(err => {
+//     return response.throw(400, "no data to get the api,");
+//   });
+
+// // logApiCallResult("campaigns api call complete.", camAdSet);
+// // logApiCallResult("campaigns api call complete.", campaigns);
+
+// adsetLogApiCallResult("adsets api call complete.", adSets);
--- facebook_ads/facebook_ads.js
+++ facebook_ads/facebook_ads.js
@@ -2,11 +2,7 @@
 
 // // const accessToken = "EAAH0vZC5H3I8BAMmqZBB0wgjyHZCVKltwXV4LVYk4eC4csZBR2JlT2qlz7oK5TKZCQHQru93mU3pmmrVsDbr4mZCMw3PYDgsk6GxlVlnc2PzGejDZAlF6YxG0UloYzTY6dz5kSxYWEQvLqNlfwM6je9ROwiAHZAegip9valSRJkp8gZDZD"; // old
 
-<<<<<<< HEAD
- const accessToken = "EAAgSjZC0XzacBAChsNn9QFmCta44IgUsOkuZCiIfjrKfvKCOblLvX0HDqQPIgNqNw2TvsSNDdABSw5pwg2GonpZArH9PP846LAGxLY7k4wdCLW9jUu9KmA4IVP1qETSg2zYFxSLHwj1FbU4KlavijqRegcs3ZB28mi4ZAI0GkgPIb4IZBH2S0yDyl5uwvUyA0ZD";
-=======
 //  const accessToken = "EAAgSjZC0XzacBAH5LvD8lErqAbW57pKOkKZA9OWbXZCbBxxvwoWRvpJZBXyOsdQwVuLqYuBA4eE4ZCDTdQmXp8WiO2W7Fm32CqouknwZAVhzPRzOKODr0sHeDrMZATIByIxylZCHpcQEL51NFgVZA3eOfdRn0hgEkUAFAmlOfkzxZBFu5V6wW37LAyAXgxXwXDm2QZD";
->>>>>>> facebook
 
 //  // const accountId = "act_2294603300601736"; // OLD
 //  //const accountId = "act_2272209449504167"; // real
