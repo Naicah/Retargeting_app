977ed40162e4bf73df66bd667efca62ff2588065
nina hedman <ni.hedman@gmail.com>
2019-05-02 14:41:52 +0200

    Calculated time left to last application day

--- public/interface/job_ads/ads.js
+++ public/interface/job_ads/ads.js
@@ -5,6 +5,21 @@ $.getJSON("/allAds", function(data) {
     data: {
       adsList: data,
       title: "Workbuster"
+    },
+    methods: {
+      calDaysLeft: function(date) {
+        const now = Date.now();
+        // const latest = new Date("2019-06-01T22:00:00Z");
+        const latest = new Date(date);
+        const millisecondsLeft = latest - now;
+        const daysLeft = Math.floor(millisecondsLeft / 1000 / 60 / 60 / 24);
+
+        if (daysLeft > 0) {
+          return daysLeft + " dagar kvar";
+        } else {
+          return "Avslutad";
+        }
+      }
     }
   });
 });
