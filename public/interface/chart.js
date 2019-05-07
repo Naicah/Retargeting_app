32d07a10f99b96d5dda339aff6d1d99e44c92c8b
Oliver Brian Barra Vasquez <oliver.vasquez@student.kyh.se>
2019-05-07 22:53:53 +0200

    added some styling to the statistics chart

--- public/interface/chart.js
+++ public/interface/chart.js
@@ -12,14 +12,54 @@ Vue.component("bar-chart", {
         labels: ["Ansökningar", "Visningar", "Klick"],
         datasets: [
           {
-            label: "",
-            backgroundColor: "blue",
+            label: "Antal:",
+            backgroundColor: [
+              "rgba(45, 125, 210, 1)",
+              "rgba(34, 95, 160, 1)",
+              "rgba(14, 38, 63, 1)"
+            ],
             data: banandata
             //data: [adLength]
           }
-        ]
+        ],
+        // options: {
+        //   legend: {
+        //     display: false
+        //   }
+        // }
       },
-      { responsive: true, maintainAspectRatio: false }
+      
+      { 
+        responsive: true, 
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false
+            },
+            ticks: {
+              display: false
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false,
+              drawBorder: false
+            }
+          }]
+        },
+        // scaleLabel: {
+        //   display: false
+        // }
+        // gridLines: {
+        //   display: false
+        // }
+
+      }
     );
   }
 });
