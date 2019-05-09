63823792b2804f1950834a0569fd0524b3f0d20c
Oliver Brian Barra Vasquez <oliver.vasquez@student.kyh.se>
2019-05-09 10:01:41 +0200

    statistics removed y and x-axis lines

--- public/interface/chart.js
+++ public/interface/chart.js
@@ -19,16 +19,11 @@ Vue.component("bar-chart", {
               "rgba(14, 38, 63, 1)"
             ],
             data: banandata
-            //data: [adLength]
           }
         ],
-        // options: {
-        //   legend: {
-        //     display: false
-        //   }
-        // }
+       
       },
-      
+      //====================== OPTIONS ========================//
       { 
         responsive: true, 
         maintainAspectRatio: false,
@@ -52,62 +47,9 @@ Vue.component("bar-chart", {
             }
           }]
         },
-        // scaleLabel: {
-        //   display: false
-        // }
-        // gridLines: {
-        //   display: false
-        // }
-
+       
       }
     );
   }
 });
 
-// var vm = new Vue({
-//   el: "#chart",
-//   data: {
-//     message: ""
-//   }
-// });
-
-/* var annons = document.getElementsByClassName("ad");
-
-function initChart() {
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: "bar",
-    // The data for our dataset
-    data: {
-      labels: ["Notes"],
-      datasets: [
-        {
-          label: "NOTES",
-          backgroundColor: "blue",
-          data: [notesLength]
-        }
-      ]
-    },
-    // Configuration options go here
-    options: {
-      scales: {
-        xAxes: [
-          {
-            barPercentage: 0.3
-          }
-        ],
-        yAxes: [
-          {
-            stacked: true
-          }
-        ]
-      }
-    }
-  });
-}
-function updateChart(adLength) {
-  chart.data.datasets[0].data = ad.adLength;
-  chart.update();
-}
- */
