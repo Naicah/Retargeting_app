567dc2197a691c7febe1565547504c20d7494989
nina hedman <ni.hedman@gmail.com>
2019-05-16 11:47:39 +0200

    Ads dynamicly show name of logged in user (no login function yet). Ads function to get all ads based on status, calculates tota amount for eacg status, displays correct value in status buttons. Ads comments to all functions in ads.js

--- public/interface/job_ads/ads.js
+++ public/interface/job_ads/ads.js
@@ -1,101 +1,83 @@
-const url = 'allAds'
+const url = "allAds";
 fetch(url)
   .then(res => res.json())
   .then(data => {
     new Vue({
-      el: '#mainContainer',
+      el: "#mainContainer",
       data: {
+        loggedInUser: "Workbuster",
         allAdsList: data,
-        adStatistics: [0, 0, 0],
         adsToShowList: [],
+        showStatus: "",
         sortContainerHidden: false,
         filterContainerHidden: false,
-        filterSearch: '',
-        filterLocation: '',
-        filterJobCategory: '',
-        filterCompany: '',
-        showStatus: '',
-        active_el: 0,
-        statisticsChart: ''
+        filterSearch: "",
+        filterLocation: "",
+        filterJobCategory: "",
+        filterCompany: "",
+        statisticsChart: "",
+        adStatistics: [0, 0, 0]
       },
       //  CALCULATES AND SAVES STATUS FOR EACH AD IN allAdsList
       created() {
-        const list = this.allAdsList
-        var i
+        const list = this.allAdsList;
+        var i;
         for (i = 0; i < list.length; i++) {
-          daysLeft = this.calcDaysLeft(list[i].last_application_timestamp)
+          daysLeft = this.calcDaysLeft(list[i].last_application_timestamp);
 
           if (daysLeft > 0) {
-            list[i].status = 'ongoing'
+            list[i].status = "ongoing";
           } else {
-            list[i].status = 'finished'
+            list[i].status = "finished";
           }
         }
-        this.showOngoing = true
       },
 
       mounted() {
-        this.showStatus = 'ongoing' // Display ongoing ads
-        this.createChart() // Display statistics
-      },
+        this.showStatus = "ongoing"; // Display ongoing ads
+        this.createChart(); // Display statistics
+      }, // End mounted
       watch: {
         // SHOW ADS BASED ON STATUS
         showStatus: function() {
-          let status
-
-          if (this.showStatus === 'all') {
-            this.adsToShowList = this.allAdsList
-          } else {
-            switch (this.showStatus) {
-              case 'ongoing':
-                status = 'ongoing'
-                break
-              case 'finished':
-                status = 'finished'
-                break
-              case 'all':
-                break
-            }
-            this.adsToShowList = this.allAdsList.filter(
-              ad => ad.status === status
-            )
-          }
+          this.adsToShowList = this.getAdsByStatus(this.showStatus);
         },
 
         // ===================== STATISTICS =============== //
         // DISPLAYS CHOSEN STATISTICS
         adStatistics: function() {
-          this.createChart()
+          this.createChart();
         }
       }, // End watch
 
       methods: {
         // TOGGLE VALUE (BOOLEAN) FOR GIVEN DATAKEY
         toggle(dataKey) {
-          this[dataKey] = !this[dataKey]
+          this[dataKey] = !this[dataKey];
         },
 
+        // ===================== STATUS ================== //
         // TOGGLE WHICH ADS THATS ARE SHOWED BASED ON STATUS
         showAdsByStatus(status) {
-          this.showStatus = status
+          this.showStatus = status;
         },
 
-        // ============== FILTER ================ //
-
+        // ==================== FILTER =================== //
+        // GET VALUE TO FILTER ON FROM INPUT FIELD, STORE IN DATA
         getFilterInput(type) {
-          this['filter' + type] = event.target.value
+          this["filter" + type] = event.target.value;
         },
 
-        // ================ SORT ================ //
-        // SORT ADS BASED ON WHICH SORT BUTTON WAS CLICKED
+        // ======================= SORT =================== //
+        // SORT ADS BASED ON WHICH SORT BUTTON WAS CLIC1KED
         sort(value, type) {
           switch (type) {
-            case 'string':
-              this.adsToShowList.sort((a, b) => (a[value] > b[value] ? 1 : -1))
-              break
-            case 'integer':
-              this.adsToShowList.sort((a, b) => b[value] - a[value])
-              break
+            case "string":
+              this.adsToShowList.sort((a, b) => (a[value] > b[value] ? 1 : -1));
+              break;
+            case "integer":
+              this.adsToShowList.sort((a, b) => b[value] - a[value]);
+              break;
           }
         },
 
@@ -105,28 +87,30 @@ fetch(url)
         // CREATES COMMA IF AD HAS A JOB CATEGORY
         ifJobCategory(ad) {
           if (ad.job_category) {
-            return ','
+            return ",";
           } else {
-            return ''
+            return "";
           }
         },
 
         // --------------- DAYS LEFT --------------- //
         // CALCULATE DAYS LEFT TO APPLY TO JOB
         calcDaysLeft(date) {
-          const now = Date.now()
-          const latest = new Date(date)
-          const millisecondsLeft = latest - now
-          return (daysLeft = Math.floor(millisecondsLeft / 1000 / 60 / 60 / 24))
+          const now = Date.now();
+          const latest = new Date(date);
+          const millisecondsLeft = latest - now;
+          return (daysLeft = Math.floor(
+            millisecondsLeft / 1000 / 60 / 60 / 24
+          ));
         },
         // RETURNS HOW MANY DAYS LEFT TO APPLY, OR SAYS THAT AD IS FINISHED
         getStatus(date) {
-          const daysLeft = this.calcDaysLeft(date)
+          const daysLeft = this.calcDaysLeft(date);
 
           if (daysLeft > 0) {
-            return daysLeft + ' dagar kvar'
+            return daysLeft + " dagar kvar";
           } else {
-            return 'Avslutad'
+            return "Avslutad";
           }
         },
 
@@ -135,34 +119,34 @@ fetch(url)
         getAdStatistics(ad) {
           this.adStatistics.length < 3
             ? this.adStatistics.push(ad.applies, ad.clicks, ad.views)
-            : this.adStatistics.splice(0, 3, ad.applies, ad.clicks, ad.views)
+            : this.adStatistics.splice(0, 3, ad.applies, ad.clicks, ad.views);
 
-          return this.adStatistics
+          return this.adStatistics;
         },
         // CREATE CHART FOR STATISTICS
         createChart() {
           // Remove previous chart
-          if (this.statisticsChart !== '') {
-            this.statisticsChart.destroy()
+          if (this.statisticsChart !== "") {
+            this.statisticsChart.destroy();
           }
           // Create new chart
-          this.statisticsChart = new Chart('bar-chart', {
-            type: 'bar',
+          this.statisticsChart = new Chart("bar-chart", {
+            type: "bar",
             data: {
-              labels: ['Ansökningar', 'Klick', 'Visningar'],
+              labels: ["Ansökningar", "Klick", "Visningar"],
               datasets: [
                 {
                   data: this.adStatistics,
-                  label: '',
+                  label: "",
                   backgroundColor: [
-                    'rgba(45, 125, 210, 1)',
-                    'rgba(34, 95, 160, 1)',
-                    'rgba(14, 38, 63, 1)'
+                    "rgba(45, 125, 210, 1)",
+                    "rgba(34, 95, 160, 1)",
+                    "rgba(14, 38, 63, 1)"
                   ],
                   // Sets where to show numbers
                   datalabels: {
-                    align: 'end',
-                    anchor: 'end'
+                    align: "end",
+                    anchor: "end"
                   }
                 }
               ]
@@ -214,65 +198,81 @@ fetch(url)
                 datalabels: {
                   // Numbers
                   font: {
-                    weight: 'bold',
+                    weight: "bold",
                     size: 25,
-                    family: 'Montserrat'
+                    family: "Montserrat"
                   },
-                  color: 'rgba(132, 133, 132, 1)'
+                  color: "rgba(132, 133, 132, 1)"
                 }
               },
               responsive: true,
               responsiveAnimationDuration: 0,
               maintainAspectRatio: false // Enables posibility to set size of chart (set in CSS)
             }
-          })
+          });
         },
-        createFilterList(dataKey) {
-          const filterList = []
+        // GET ARRAY OF ADS WITH THE GIVEN STATUS
+        getAdsByStatus(status) {
+          if (status === "all") {
+            ads = this.allAdsList;
+          } else {
+            ads = this.allAdsList.filter(ad => ad.status === status);
+          }
+          return ads;
+        },
+        // GET ARRAY OF KEYS IN AD TO BE ABLE TO FILTER ON (only unique values)
+        getFilterList(dataKey) {
+          const filterList = [];
           this.adsToShowList.map(ad => {
             if (!filterList.includes(ad[dataKey])) {
-              filterList.push(ad[dataKey])
+              filterList.push(ad[dataKey]);
             }
-          })
-          return filterList
+          });
+          return filterList;
         }
       }, // End methods
 
       computed: {
-        // ================ FILTER ================== //
+        // ===================== STATUS ==================== //
+        // CALCULATE TOTAL ADS WITH STATUS ONGOING
+        totalOngoing: function() {
+          return this.getAdsByStatus("ongoing").length;
+        },
+        // CALCULATE TOTAL ADS WITH STATUS FINISHED
+        totalFinished: function() {
+          return this.getAdsByStatus("finished").length;
+        },
+        // CALCULATE TOTAL ADS
+        totalAll: function() {
+          return this.getAdsByStatus("all").length;
+        },
+        // ===================== FILTER ===================== //
         // SHOW ADS THAT MATCHES SEARCH
         filterSearchList() {
           return this.adsToShowList.filter(ad => {
             return ad.title
               .toLowerCase()
-              .includes(this.filterSearch.toLowerCase())
-          })
+              .includes(this.filterSearch.toLowerCase());
+          });
         },
-        // CREATES LIST OF ALL LOCATIONS
-        adLocationList: function() {
-
-          adLocation = this.createFilterList('city')
-          return adLocation
-         
-        },
-        // CREATES LIST OF ALL JOB CATEGORIES
-        adJobCategoryList: function() {
-          adJobCategory = this.createFilterList('job_category')
-          return adJobCategory
-        },
-        // CREATES LIST OF ALL COMPANIES
+        // CREATES FILTERLIST OF ALL COMPANIES
         adCompaniesList: function() {
-
-          adCompany = this.createFilterList('company')
-          return adCompany
+          adCompany = this.getFilterList("company");
+          return adCompany;
+        },
+        // CREATES FILTERLIST OF ALL LOCATIONS
+        adLocationList: function() {
+          adLocation = this.getFilterList("city");
+          return adLocation;
+        },
+        // CREATES FILTERLIST OF ALL JOB CATEGORIES
+        adJobCategoryList: function() {
+          adJobCategory = this.getFilterList("job_category");
+          return adJobCategory;
         }
-      }
-    })
-    // ========================================================= //
-    //               STATISTIC MENU                              //
-    //         INIT A CHART FOR STATISTIC                         //
-    // ========================================================= //
+      } // End computed
+    });
   })
   .catch(err => {
-    throw err
-  })
+    throw err;
+  });
